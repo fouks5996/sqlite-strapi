23e3ff26d380986fb6bc966f1b83d78c9c8d077e
foucauld <foucauldbeaumont@yahoo.fr>
2023-05-12 01:04:54 +0100

    add cloudinary

---
+++ config/env/production/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "dgbqiwyyb",
+        api_key: "199852999367284",
+        api_secret: "i8UIF-SLqe6CRQpifScvgIkxadc",
+      },
+    },
+  },
+});
